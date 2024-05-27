3409eebc9a5bc896d60584bbfffacbd30fdc6f0f
Fadhil <88182212+FadhilHere@users.noreply.github.com>
2024-05-27 22:21:13 +0700

    tes git

--- resources/views/kuliah.blade.php
+++ resources/views/kuliah.blade.php
@@ -209,7 +209,7 @@
                                             <div class="row">
                                                 <div class="col-md-6">
                                                     <div class="form-group">
-                                                        <label for="nama">Nama kamuh:</label>
+                                                        <label for="nama">Nama:</label>
                                                         <input type="text" name="nama" id="nama"
                                                             class="form-control" required>
                                                     </div>
